d74b4c11183e2e3fecc953938f047e0f6d7d8c4f
Patrick-Emil Zörner <paddyez@gmail.com>
2018-04-30 20:53:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import org.paddy.streams.CollectionManipulation;
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        CollectionManipulation fl = new CollectionManipulation();
+    }
+}
---
+++ src/org/paddy/streams/CollectionManipulation.java
@@ -0,0 +1,45 @@
+package org.paddy.streams;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+public class CollectionManipulation {
+    private static final String exclude = "Walter";
+    private List<String> names = Arrays.asList("Patrick", "Christoph", "Andreas", exclude);
+    public CollectionManipulation() {
+        filterListByExclude();
+    }
+    private void filterListByExclude() {
+        System.out.println("Paradigm style:");
+        for(String name : names) {
+            if(!name.equals(exclude)) {
+                System.out.println(name);
+            }
+        }
+        System.out.println("Functional style 1:");
+        names.stream()
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(String name) {
+                        return !name.equals(exclude);
+                    }
+                })
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String name) {
+                        System.out.println(name);
+                    }
+                });
+        System.out.println("Functional style lambda 2:");
+        names.stream()
+                .filter(name -> !name.equals(exclude))
+                .forEach(name -> System.out.println(name));
+        System.out.println("Functional style lamda 3:");
+        names.stream()
+                .filter(CollectionManipulation::isNotWalter)
+                .forEach(System.out::println);
+    }
+    private static boolean isNotWalter(String name) {
+        return !name.equals(exclude);
+    }
+}
