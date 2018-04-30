387a1237bd2ffaaa21ae28a66822e27192245621
Patrick-Emil Zörner <paddyez@gmail.com>
2018-04-30 20:59:21 +0200

    Cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import org.paddy.streams.CollectionManipulation;
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello World!");
-        CollectionManipulation fl = new CollectionManipulation();
+        CollectionManipulation collManip = new CollectionManipulation();
     }
 }
